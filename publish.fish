08bdfde35ed6a51ca2bebda0c41f0da9297f6cfe
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-11-15 00:22:23 +0100

    new v0.3.2-2

--- publish.fish
+++ publish.fish
@@ -1,6 +1,6 @@
 set newversion (sed -n 's/^version = "\(.*\)"$/\1/p' pyproject.toml)
 git commit -am "new v$newversion"
 git tag -a v$newversion -m "v$newversion"
-git push origin v$newversion
+git push --tags
 uv build
 uv publish --username __token__ --password (rbw get "pypi" "00sapo" -f "token")
