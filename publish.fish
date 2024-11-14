2220e913182555d0e0da36e613eb87171fd40589
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2024-11-14 19:11:26 +0100

    test

--- publish.fish
+++ publish.fish
@@ -1,2 +1,6 @@
+version=sed -n 's/^version = "\(.*\)"$/\1/p' pyproject.toml
+git commit -am "new v$version"
+git tag -a v$version -m "v$version"
+git push origin v$version
 uv build
 uv publish --username __token__ --password (rbw get "pypi" "00sapo" -f "token")
